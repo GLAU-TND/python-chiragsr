ecbb23c911cedf6be06f06063f2fd5f2bcb000d0
chiragsr <chirag.srivastava_cs17@gla.ac.in>
2019-08-30 18:01:13 +0530

    without loop solution

--- bottlenecksolution.py
+++ bottlenecksolution.py
@@ -1,9 +1,8 @@
+#from collections import Counter
+
 bottles = int(input("No. of bottles"))
 radius = list(map(int, input("Enter radius here").split()))
 
-min_radius = min(radius)
-l = []
-for x in radius:
-    if(min_radius > x):
-        radius.remove(x)
 
+res_list = (max([radius.count(i) for i in radius]))
+print(res_list)
